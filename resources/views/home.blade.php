1c85f20d7bac0e2caa2b6154a909bc11e17c2284
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-10-04 20:58:55 +0200

    Mise en place des vues. Correction d'un problème sur la modal de connexion par le lien "vous vendez!"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
     <div class="row">
 
         <div class="col-12 col-md-6 flex-column flex-center" style="height:150px">
-            <a id="buybtn1" href="" @click.prevent="show = !show">
+            <a id="buybtn1" href="">
                 <i class="fas fa-shopping-basket fa-4x flex-center"></i>
                 <p class="lead text-center">Vous achetez !</p>
             </a>
@@ -26,7 +26,7 @@
         
         
         <div class="col-12 col-md-6 flex-column flex-center " style="height:150px">
-                <a class="links" href="#logInModal" data-toggle="modal" data-target="#logInModal" >
+                <a href="" data-toggle="modal" data-target="#logInModal">
                     <i class="fas fa-home fa-4x flex-center"></i>
                     <p class="lead ">Vous vendez !</p>
                 </a>
@@ -34,7 +34,7 @@
             
     </div>
 
-    <div><button >Fermer la carte</button></div>
+    
 
     <div class="col-12 col-lg-6 offset-lg-3" id="mapbox" style="height:85vh;width:100vw">
         <l-map :zoom="zoom" :center="center">
@@ -42,8 +42,6 @@
             <l-marker :lat-lng="marker"></l-marker>
         </l-map>
     </div>
-
-
     
 </div>
 @endsection
