ded04630fb7802709869a08bb5ba19996ef46873
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-10-04 18:37:23 +0200

    Création des models Category et Comment.

---
+++ app/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+        'user_id','seller_id','title','content',
+    ];
+
+    public function seller()
+    {
+        return $this->belongsTo('App\Seller');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
