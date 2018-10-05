20a2f010fe149ae7f4c4e33ac4fec5e88d040841
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-10-05 20:28:58 +0200

    Mise en place des icones de produits. Mise en place du toggle au clic sur le bouton "achetez". Définition d'un media queries pour rendre la map full-screen sur mobile.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('contenu')
 <div>
-    <div>
+    <div id="titleblock" class="collapse show multi-collapse">
 
         <h1 class="display-6 text-center mt-2">Iticourt</h1>
         <p class="lead text-center container">L'itinéraire des circuits courts</p>
@@ -14,14 +14,14 @@
     
     <div class="row">
 
-        <div class="col-12 col-md-6 flex-column flex-center" style="height:150px">
-            <a id="buybtn1" href="">
+        <div class="col-12 col-md-6 flex-column flex-center multi-collapse collapse show" style="height:150px">
+            <a id="buybtn1" href="" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="signinlink titleblock">
                 <i class="fas fa-shopping-basket fa-4x flex-center"></i>
                 <p class="lead text-center">Vous achetez !</p>
             </a>
         </div>        
         
-        <div class="col-12 col-md-6 flex-column flex-center " style="height:150px">
+        <div id="signinlink" class="col-12 col-md-6 flex-column flex-center collapse show multi-collapse" aria-expanded="false" style="height:150px">
                 <a href="" data-toggle="modal" data-target="#logInModal">
                     <i class="fas fa-home fa-4x flex-center"></i>
                     <p class="lead ">Vous vendez !</p>
@@ -30,11 +30,19 @@
             
     </div>
 
-    <div class="col-12 col-lg-6 offset-lg-3" id="mapbox" style="height:85vh;width:100vw">
-        <l-map :zoom="zoom" :center="center">
-            <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-            <l-marker :lat-lng="marker"></l-marker>
-        </l-map>
+    <!-- les icones de produits -->
+    <div class="container my-2 d-flex justify-content-around col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-3 ">
+        <ul class="nav collapse multi-collapse">
+            <li class="nav-item"><img class="imagecat" src="/storage/svg/carrot-and-apple.svg" alt='carrot' width="50px" height="50px"/></li>
+            <li class="nav-item"><img class="imagecat" src="/storage/svg/animals.svg" alt='animals' width="50px" height="50px"/></li>
+            <li class="nav-item"><img class="imagecat" src="/storage/svg/milk.svg" alt='milk' width="50px" height="50px"/></li>
+            <li class="nav-item"><img class="imagecat" src="/storage/svg/wine.svg" alt='wine' width="50px" height="50px"/></li>
+            <li class="nav-item"><a href="" data-toggle="collapse" data-target="multi-collapse" aria-expanded="true" aria-controls="signinlink titleblock"><img class="imagecat" src="/storage/svg/cancel.svg" alt='cancel' width="50px" height="50px"/></a></li>
+        </ul>
+    </div>
+
+    <div class="col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-3 collapse multi-collapse" id="mapbox" style="">
+        <div style="height:75vh;background-color:blue"></div>
     </div>
     
 </div>
