a540cd748a51efcbc6c56a05c5c2b43a9c69da86
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-10-05 09:34:54 +0200

    Modification de la vue home. Retrait du paragraphe inutile.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,7 @@
         <p class="lead text-center container">L'itinéraire des circuits courts</p>
         <hr>
 
-        <div class="row d-none d-md-block mt-5">
-            <p class="lead text-center flex-center" style="height:150px">Que voulez-vous faire ?</p>        
-        </div>
+        <div class="row d-none d-md-block mt-5" style="height:150px"></div>
 
     </div>
     
@@ -21,9 +19,7 @@
                 <i class="fas fa-shopping-basket fa-4x flex-center"></i>
                 <p class="lead text-center">Vous achetez !</p>
             </a>
-        </div>
-
-        
+        </div>        
         
         <div class="col-12 col-md-6 flex-column flex-center " style="height:150px">
                 <a href="" data-toggle="modal" data-target="#logInModal">
@@ -34,8 +30,6 @@
             
     </div>
 
-    
-
     <div class="col-12 col-lg-6 offset-lg-3" id="mapbox" style="height:85vh;width:100vw">
         <l-map :zoom="zoom" :center="center">
             <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
@@ -44,4 +38,5 @@
     </div>
     
 </div>
+
 @endsection
