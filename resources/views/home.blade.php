49a3423288867e8ba4dc6d54d0215da766abaa9e
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-10-06 13:38:22 +0200

    Amélioration de la page d'accueil pour l'animation d'apparition de carte et d'icones. Ajout de la debug bar laravel.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,16 +14,16 @@
     
     <div class="row">
 
-        <div class="col-12 col-md-6 flex-column flex-center multi-collapse collapse show" style="height:150px">
+        <div id="buyingblock" class="col-12 col-md-6 flex-column flex-center multi-collapse collapse show">
             <a id="buybtn1" href="" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="signinlink titleblock">
-                <i class="fas fa-shopping-basket fa-4x flex-center"></i>
+                <i class="fas fa-shopping-basket fa-5x flex-center"></i>
                 <p class="lead text-center">Vous achetez !</p>
             </a>
-        </div>        
+        </div>
         
-        <div id="signinlink" class="col-12 col-md-6 flex-column flex-center collapse show multi-collapse" aria-expanded="false" style="height:150px">
+        <div id="signinlink" class="col-12 col-md-6 flex-column flex-center collapse show multi-collapse" aria-expanded="false">
                 <a href="" data-toggle="modal" data-target="#logInModal">
-                    <i class="fas fa-home fa-4x flex-center"></i>
+                    <i class="fas fa-home fa-5x flex-center"></i>
                     <p class="lead ">Vous vendez !</p>
                 </a>
         </div>
@@ -31,13 +31,13 @@
     </div>
 
     <!-- les icones de produits -->
-    <div class="container my-2 d-flex justify-content-around col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-3 ">
-        <ul class="nav collapse multi-collapse">
+    <div class="container my-2  col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-3 ">
+        <ul class="nav collapse multi-collapse justify-content-around">
             <li class="nav-item"><img class="imagecat" src="/storage/svg/carrot-and-apple.svg" alt='carrot' width="50px" height="50px"/></li>
             <li class="nav-item"><img class="imagecat" src="/storage/svg/animals.svg" alt='animals' width="50px" height="50px"/></li>
             <li class="nav-item"><img class="imagecat" src="/storage/svg/milk.svg" alt='milk' width="50px" height="50px"/></li>
             <li class="nav-item"><img class="imagecat" src="/storage/svg/wine.svg" alt='wine' width="50px" height="50px"/></li>
-            <li class="nav-item"><a href="" data-toggle="collapse" data-target="multi-collapse" aria-expanded="true" aria-controls="signinlink titleblock"><img class="imagecat" src="/storage/svg/cancel.svg" alt='cancel' width="50px" height="50px"/></a></li>
+            <li class="nav-item"><a href="" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="true" aria-controls="signinlink titleblock"><img class="imagecat" src="/storage/svg/cancel.svg" alt='cancel' width="50px" height="50px"/></a></li>
         </ul>
     </div>
 
