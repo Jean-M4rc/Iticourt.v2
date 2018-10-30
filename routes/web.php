28d0f4d0e0fad5c186a0b01d808514c1de4fb6eb
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-10-30 10:54:10 +0100

    Purge du fichier de route. Purge des fichiers terminée.

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::view('/', 'home');
 
-Route::view('/about','about');
+Route::view('/about', 'about');
 
 Route::get('/inscription', 'InscriptionController@formulaire');
 
@@ -23,16 +23,13 @@ Route::post('/inscriptionAcheteur', 'InscriptionController@traitementAcheteur');
 
 Route::post('/connexion', 'ConnexionController@traitement');
 
-Route::get( '/sellersList' , 'SellerListController@sellersList');
+Route::get('/sellersList', 'SellerListController@sellersList');
 
 Route::get('/sellersMarkersGet', 'SellerListController@sellersMarkers');
 
 Route::get('/sellerFile{id}', 'SellerListController@sellerFile');
 
-Route::group([
-    'middleware' => 'App\Http\Middleware\Auth'
-
-    ], function () {
+Route::group(['middleware' => 'App\Http\Middleware\Auth'], function () {
 
     Route::get('/rootme', 'RootController@administration');
 
@@ -52,7 +49,7 @@ Route::group([
 
     Route::post('/comments', 'CommentsController@store');
 
-    Route::get('/deconnexion', 'CompteController@deconnexion'); 
+    Route::get('/deconnexion', 'CompteController@deconnexion');
 
     Route::post('/deleteMyAccount', 'CompteController@deleteMyAccount');
 
